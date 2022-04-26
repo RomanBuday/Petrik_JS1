4a7d4fabe107cd38a55712674b07d8af08da841e
Roman Budai <roman1985buday@gmail.com>
2022-04-26 17:50:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "18");
+// console.log(answer + 5);
+
+// let answers = [];
+
+// answers[0] = prompt('your name?', '');
+// answers[1] = prompt('your surname?', '');
+// answers[2] = prompt('your age?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// console.log(`Hello, ${user}`);
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    // ++incr;
+    // --decr;
+
+    console.log(incr++);
+    console.log(decr--);
